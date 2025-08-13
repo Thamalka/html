de0ebc100954a4128b7e1e04b13841a69d8b75a9
Thamalka <thamalkapiumi@gmail.com>
2025-08-13 12:26:20 +0530

    Add files via upload

---
+++ java1.js
@@ -0,0 +1,20 @@
+document.getElementById("box").innerHTML="webdiv";
+console.log("Display Output");
+window.alert("Click Ok");
+console.log(10+60);
+let a=10;
+let b=20;
+let c="sum";
+let d=a+b;
+const value=100;
+
+console.log(c+d);
+console.log(value);
+document.getElementById("output").innerHTML = c+d;
+
+let text = "";
+    for (let i =0; i<5; i++){
+        text += "The number is " + i + "<br>";
+    }
+     document.getElementById("demo").innerHTML = text;
+
